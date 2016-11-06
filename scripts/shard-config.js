b19994e805be7a802add982b87ca14cc4d17621a
Victor <victorsvv@yahoo.com>
2016-11-06 12:19:48 +0100

    Shard collection script.

--- scripts/shard-config.js
+++ scripts/shard-config.js
@@ -1,3 +1,6 @@
 sh.addShard( "localhost:30101" );
 sh.addShard( "localhost:30102" );
 sh.addShard( "localhost:30103" );
+use samples
+sh.enableSharding("samples")
+sh.shardCollection("samples.coll",{_id:1})
