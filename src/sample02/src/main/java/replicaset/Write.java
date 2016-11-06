580514977339d072939e71aa9295ddbfd728404e
Victor <vitongos@gmail.com>
2016-11-06 10:51:17 +0100

    Rename sample02.

--- src/sample02/src/main/java/replicaset/Write.java
+++ src/sample02/src/main/java/replicaset/Write.java
@@ -1,24 +1,29 @@
-package mongodb.sample02;
+package replicaset;
 
 import java.net.UnknownHostException;
 
 import org.bson.Document;
 
-import com.mongodb.MongoClient;
-import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoCollection;
+import com.mongodb.*;
+import com.mongodb.client.*;
 
-public class Read {
-	public static void main( String[] args ) throws UnknownHostException, InterruptedException
+/**
+ * Hello world!
+ *
+ */
+public class Write 
+{
+    public static void main( String[] args ) throws UnknownHostException, InterruptedException
     {
-        @SuppressWarnings("resource")
+    	@SuppressWarnings("resource")
 		MongoClient client = new MongoClient(new ServerAddress("localhost", 30001));
         MongoCollection<Document> collection = client.getDatabase("samples").getCollection("ids");
+        collection.drop();
         
         for (int i = 0; i < Integer.MAX_VALUE; i++) {
         	try {
-        		Document document = collection.find().sort(new Document("_id", -1)).first();
-        		System.out.println("Readed : " + document.get("_id").toString());
+        		collection.insertOne(new Document("_id", i));
+        		System.out.println("Inserted : " + i);
         	}
         	catch (Exception exception) {
         		System.out.println(exception.getMessage());
